e12b5f3efd4897f5678f558f00aad791ccf17b81
Ahmet <mrkronoss@hotmail.com>
2022-11-30 12:24:59 +0300

    fix: fix

--- src/ToDoReducer/Reducer.js
+++ src/ToDoReducer/Reducer.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function Reducer() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  const submitHandle = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("")
+  };
   return (
-    <div>Reducer</div>
-  )
+    <>
+      <h1>useReducer()</h1>
+      <form onSubmit={submitHandle}>
+        <input
+          type={"text"}
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        ></input>
+        <button disabled={!todo} type="submit">Ekle</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
